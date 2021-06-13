2a47978d12956d0b000b49fab215f0e1df4416fe
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2021-06-13 17:57:13 +0800

    Update hacash.sh

--- hacash.sh
+++ hacash.sh
@@ -4,6 +4,7 @@ apt install git -y
 git clone https://github.com/yuqingkj/minehac.git
 cd minehac/
 chmod 777 ./hacash_miner
+screen -dmS hac
 screen -x -S hac -p 0 -X stuff "./hacash_miner \n"
 echo ok
 EOF
